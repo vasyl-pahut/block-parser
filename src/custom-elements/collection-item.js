d679fcf9acf910d3247af3495208b6b2764bf5d3
Jantwel <pahutv@weblium.com>
2017-12-25 17:46:34 +0200

    fix slider item

--- src/custom-elements/collection-item.js
+++ src/custom-elements/collection-item.js
@@ -4,7 +4,7 @@ window.customElements.define('app-children-placeholder', ChildrenPlaceholder)
 const CollectionItem = {
   getElements: () => null,
   convert: ({dom, bind}) => {
-    if (bind.includes('collection') || bind.includes('slider')) {
+    if (bind.includes('collection')) {
       const childrenPlaceholder = new ChildrenPlaceholder()
       childrenPlaceholder.innerText = '{children}'
       dom.children[0].prepend(childrenPlaceholder)
